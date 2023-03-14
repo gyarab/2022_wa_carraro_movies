4075116fb159f5de46a92fe5077621a72aec701b
NellaCarraro <nella.carraro@student.gyarab.cz>
2023-03-14 11:16:22 +0100

    14.03.

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from.models import Movies
-admin.site.register(Movies)
-# Register your models here.
+
+# Register your models here
+class MoviesAdmin(admin.ModelAdmin):#trida 
+    list_display=['id','name','year']#ktere atributy budou zobrazene
+    list_display_links=['name']# ktere atributy budou klikaci
+    search_fields=['id','name','description']#podle ceho budu moct vyhledavat
+admin.site.register(Movies,MoviesAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -5,4 +5,7 @@ class Movies(models.Model):
     year = models.IntegerField()
     description = models.TextField()
 
+    def __str__(self):#def metoda __str__ jmeno metody (self) jako prvni metoda v pythonu dostava jako parametr sama sebe
+        return f"{self.name}({self.year})"
+
 # Create your models here.
