895c0e49ac7cf41aa1fc37a2dfca58c5623ac24d
NellaCarraro <nella.carraro@student.gyarab.cz>
2023-03-21 11:20:59 +0100

    21.03.

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from.models import Movies
+from.models import Director
+from.models import Genre
 
 # Register your models here
 class MoviesAdmin(admin.ModelAdmin):#trida 
-    list_display=['id','name','year']#ktere atributy budou zobrazene
+    list_display=['id','name','year','director']#ktere atributy budou zobrazene
     list_display_links=['name']# ktere atributy budou klikaci
+    list_filter=['genres']
     search_fields=['id','name','description']#podle ceho budu moct vyhledavat
 admin.site.register(Movies,MoviesAdmin)
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display=['id','name','birth_year']
+    list_display_links=['name']
+admin.site.register(Director,DirectorAdmin)
+admin.site.register(Genre)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -1,11 +1,25 @@
-from django.db import models
+from django.db import models# models = reprezentace databaze
 
 class Movies(models.Model):
     name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField()
+    year = models.IntegerField(blank=True,null=True)
+    description = models.TextField(blank=True)#blank=true je ze neni povinne
+    director = models.ForeignKey('Director',blank=True,null=True,on_delete=models.SET_NULL)
+    genres = models.ManyToManyField('Genre')#manytomanyfield propoji ty tabulky n:m
 
     def __str__(self):#def metoda __str__ jmeno metody (self) jako prvni metoda v pythonu dostava jako parametr sama sebe
         return f"{self.name}({self.year})"
+    def genre_display(self):
+        return ", ".join([i.name for i in self.genres.all()])
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.IntegerField(blank=True,null=True)
 
+    def __str__(self):
+        return f"{self.name}({self.birth_year})"
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
 # Create your models here.
