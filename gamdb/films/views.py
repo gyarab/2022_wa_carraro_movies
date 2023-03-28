dbca721abb2985d443fdf1c2af5dcc5fc0dee39a
NellaCarraro <nella.carraro@student.gyarab.cz>
2023-03-28 11:20:14 +0200

    2803

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movies
 
-# Create your views here.
+def homepage(request):
+    #return HttpResponse("HELLOOO")
+    context ={
+        'movies':Movies.objects.all(),
+    }
+    return render(request,'main.html',context)
+
--- gamdb/gamdb/urls.py
+++ gamdb/gamdb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from films.views import homepage
 
-urlpatterns = [
+urlpatterns = [#rozcestik ktera funkce bude obsluhovat ktery url
     path('admin/', admin.site.urls),
+    path('',homepage)
 ]
