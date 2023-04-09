eae3c31527f20076517a7cdf5d122d53e1c5dcf8
NellaCarraro <83387233+NellaCarraro@users.noreply.github.com>
2023-04-09 16:23:30 +0200

    Update views.py
    x

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Movies
+from .models import Movies, Director
+
+def directors(request):
+    context = {
+        'logic': True,
+        'title': "Nejoblíbenější režiséři",
+        'directors': Director.objects.all()
+    }
+    print(context)
+    return render(request, 'directors.html', context)
 
 def homepage(request):
     #return HttpResponse("HELLOOO")
