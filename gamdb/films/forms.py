b1832815544679c0702d2f9e85bbb2c49075c864
NellaCarraro <nella.carraro@student.gyarab.cz>
2023-05-17 09:31:04 +0200

    x

--- gamdb/films/forms.py
+++ gamdb/films/forms.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator,MaxValueValidator
 
 class CommentForm(forms.Form):
     author = forms.CharField(required=False)
-    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5,"class=":"form-control"})#dela z mensiho policka textfield(vytvori widget),rows ja velky
+    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5,"class=":"form-control"}))#dela z mensiho policka textfield(vytvori widget),rows ja velky
     rating = forms.IntegerField(required=False,
     validator=[MinValueValidator(1),MaxValueValidator(5)])#validator pro data
